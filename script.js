6655e8ed9dec42ed3fe4e3d570956913f828979d
James <Quangvo55@gmail.com>
2015-11-25 17:29:21 -0800

    refactored code for readability

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 // No tabs or host permissions needed!
   chrome.tabs.executeScript({
     code: 
-    	'function remove(element, index, array) { element.remove(); } function setBackgroundToNull(element, index, array) { element.style.backgroundImage = ""; } var imgs = document.getElementsByTagName("img"); Array.prototype.slice.call(imgs).forEach(remove); var divs = document.getElementsByTagName("div"); Array.prototype.slice.call(divs).forEach(setBackgroundToNull); var headers = document.getElementsByTagName("header"); Array.prototype.slice.call(headers).forEach(remove);'
+    	'function removeElement(element, index, array) { element.remove(); }' +  
+    	'function setBackgroundToNull(element, index, array) { element.style.backgroundImage = ""; }' +
+
+    	'var imgs = document.getElementsByTagName("img");' +
+    	'Array.prototype.slice.call(imgs).forEach(removeElement);' + 
+
+    	'var divs = document.getElementsByTagName("div");' +
+    	'Array.prototype.slice.call(divs).forEach(setBackgroundToNull);' +
+    	
+    	'var headers = document.getElementsByTagName("header");' +
+    	'Array.prototype.slice.call(headers).forEach(removeElement);'
   });
 });
